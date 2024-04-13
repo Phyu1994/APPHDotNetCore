aa2b049f379b2f4541d1dc8a2af44f1d3b7b9522
Phyu1994 <163010551+Phyu1994@users.noreply.github.com>
2024-04-13 20:47:55 +0630

    Add files via upload
    [Complete] Ado.Net CRUD

---
+++ APPHDotNetCore/APPHDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using APPHDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello World!");
+
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.DataSource = "RV-IT-LP-200";
+//stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+//stringBuilder.UserID = "Bankuser";
+//stringBuilder.Password = "Abc@12345";
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+//connection.Open();
+//Console.WriteLine("Connection open.");
+
+//string query = "select * from tbl_Blog";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//sqlDataAdapter.Fill(dt);
+
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+//    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+//    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+//    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+//}
+
+//connection.Close();
+//Console.WriteLine("Connection close.");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(11, "test title", "test author", "test content");
+//adoDotNetExample.Delete(12);
+adoDotNetExample.Edit(1);
+
+Console.ReadLine();
+
