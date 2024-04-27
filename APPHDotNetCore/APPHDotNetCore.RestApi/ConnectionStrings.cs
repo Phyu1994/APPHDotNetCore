8811e171b4cf3bc6361845ca8c2d483feb54821a
aungphyuphyuhtun <aungphyuphyuhtun@gmail.com>
2024-04-27 12:17:29 +0630

    Complete DotNetCore.RestApi

---
+++ APPHDotNetCore/APPHDotNetCore.RestApi/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPHDotNetCore.RestApi
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringbuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
