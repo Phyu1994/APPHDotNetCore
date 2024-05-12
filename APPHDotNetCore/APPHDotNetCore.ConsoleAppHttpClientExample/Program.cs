b4fc5e0d131b31f79538ecfeb457077c98b17324
aungphyuphyuhtun <aungphyuphyuhtun@gmail.com>
2024-05-12 14:37:33 +0630

    Console App CRUD with API using HttpClient

---
+++ APPHDotNetCore/APPHDotNetCore.ConsoleAppHttpClientExample/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using APPHDotNetCore.ConsoleAppHttpClientExample;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+//HttpClient client = new HttpClient();
+//var response = await client.GetAsync("https://localhost:7037/api/blog");
+//if (response.IsSuccessStatusCode)
+//{
+//    string jsonStr = await response.Content.ReadAsStringAsync();
+//    Console.WriteLine(jsonStr);
+//    List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
+//    foreach(var blog in lst)
+//    {
+//        Console.WriteLine(JsonConvert.SerializeObject(blog));
+//        Console.WriteLine($"Title => {blog.BlogTitle}");
+//        Console.WriteLine($"Author => {blog.BlogAuthor}");
+//        Console.WriteLine($"Content => {blog.BlogContent}");
+
+//    }
+//}
+
+HttpClientExample httpClientExample = new HttpClientExample();
+await httpClientExample.RunAsync();
+
+Console.ReadLine();
