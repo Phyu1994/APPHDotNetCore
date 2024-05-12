841630a8b0af8a74fe092655ebfca94c07b3d27a
aungphyuphyuhtun <aungphyuphyuhtun@gmail.com>
2024-05-12 14:39:37 +0630

    Console App CRUD with API using RestClient

---
+++ APPHDotNetCore/APPHDotNetCore.ConsoleAppRestClientExamples/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using APPHDotNetCore.ConsoleAppRestClientExamples;
+
+Console.WriteLine("Hello, World!");
+
+RestClientExample restClientExamples = new RestClientExample();
+await restClientExamples.RunAsync();
+
+Console.ReadLine();
