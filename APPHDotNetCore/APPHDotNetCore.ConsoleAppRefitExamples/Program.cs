11a7214188e43299abfb7d7dd142b2d89342f917
aungphyuphyuhtun <aungphyuphyuhtun@gmail.com>
2024-05-21 15:53:18 +0630

    Console App CRUD with API using Refit

---
+++ APPHDotNetCore/APPHDotNetCore.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using APPHDotNetCore.ConsoleAppRefitExamples;
+using Refit;
+
+//try
+//{
+//    RefitExample refitExample = new RefitExample();
+//    await refitExample.RunAsync();
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.ToString());
+//}
+
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
