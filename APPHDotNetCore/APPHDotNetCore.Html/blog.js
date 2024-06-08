698fa44252b14363081aa7c5aeb9705608dc306d
aungphyuphyuhtun <aungphyuphyuhtun@gmail.com>
2024-06-08 23:10:51 +0630

    [Complete] Javascript Blog CRUD

---
+++ APPHDotNetCore/APPHDotNetCore.Html/blog.js
@@ -0,0 +1,93 @@
+const tblBlog = "blogs";
+
+//readBlog();
+//createBlog();
+//updateBlog("302b53f5-de15-4482-aee7-e6c3f1b9cf5a",'pp','pp','pp');
+deleteBlog("6beee976-f298-4570-b281-dc5fc25dea6f");
+
+function readBlog(){
+    const blogs = localStorage.getItem(tblBlog);
+    console.log(blogs);
+}
+
+function createBlog(){
+
+    const blogs = localStorage.getItem(tblBlog);
+    console.log(blogs);
+
+    let lst = [];
+    if(blogs !== null){
+        lst = JSON.parse(blogs);
+    }
+
+    const requestModel = {
+        id: uuidv4(),
+        title: "test title",
+        author: "test author",
+        content: "test content"
+    };
+
+    lst.push(requestModel);
+
+    const jsonBlog = JSON.stringify(lst);
+    localStorage.setItem(tblBlog,jsonBlog);
+}
+
+function updateBlog(id,title,author,content){
+    const blogs = localStorage.getItem(tblBlog);
+    console.log(blogs);
+
+    let lst = [];
+    if(blogs !== null){
+        lst = JSON.parse(blogs);
+    }
+
+    
+    const items = lst.filter(x => x.id === id);
+    console.log(items);
+
+    console.log(items.length);
+
+    if(items.length == 0){
+        console.log("No data found");
+        return;
+    }
+
+    const item = items[0];
+    item.title = title;
+    item.author = author;
+    item.content = content;
+
+    const index = lst.findIndex(x => x.id === id);
+    lst[index] = item;
+
+    const jsonBlog = JSON.stringify(lst);
+    localStorage.setItem(tblBlog,jsonBlog);
+}
+
+function deleteBlog(id){
+    const blogs = localStorage.getItem(tblBlog);
+    console.log(blogs);
+
+    let lst = [];
+    if(blogs !== null){
+        lst = JSON.parse(blogs);
+    }
+
+    const items = lst.filter(x => x.id === id);
+    if(items.length == 0){
+        console.log("No data found");
+        return;
+    }
+
+    lst = lst.filter(x => x.id !== id);
+    const jsonBlog = JSON.stringify(lst);
+    localStorage.setItem(tblBlog,jsonBlog);
+}
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+
+}
