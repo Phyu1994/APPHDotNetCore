dc592195e1b3dda8fb110d7c212d19462abd7e2c
aungphyuphyuhtun <aungphyuphyuhtun@gmail.com>
2024-06-18 20:00:29 +0630

    [Complete] Minimal Api CRUD

---
+++ APPHDotNetCore/APPHDotNetCore.MinimalApi/Program.cs
@@ -0,0 +1,38 @@
+using APPHDotNetCore.MinimalApi.Db;
+using APPHDotNetCore.MinimalApi.Features.Blog;
+using APPHDotNetCore.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+
+}, ServiceLifetime.Transient,
+ServiceLifetime.Transient);
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World");
+
+app.MapBlogs();
+
+app.Run();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
